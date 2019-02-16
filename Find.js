2e2e97c28b8097ca286f13a344600638baa721a9
BaalajiVS <v.s.baalaji95@gmail.com>
2019-02-16 18:32:20 -0500

    js stopword removal

--- Find.js
+++ Find.js
@@ -2,8 +2,6 @@
 (function(){
     // don't replace text within these tags
     var skipTags = { 'a': 1, 'style': 1, 'script': 1, 'iframe': 1 };
-    //https://github.com/Yoast/YoastSEO.js/blob/develop/src/config/stopwords.js
-    var stopwords = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ];
 
     // find text nodes to apply replFn to
     var findKW = function ( el, term, replFn ) {
@@ -57,7 +55,6 @@
             walkDOM(node, func);
             node = node.nextSibling;
         }
-
     };
 
     walkDOM(document.body, function (node) {
@@ -69,29 +66,15 @@
 
             text = text.split(' ');
             text = [...new Set(text)];
-            /*
-            if (text.length) {
 
-                for (var i = 0, length = text.length; i < length; i += 1) {
-                    var matched = false,
-                        word = text[i];
+            //https://github.com/Yoast/YoastSEO.js/blob/develop/src/config/stopwords.js
+            var stopwords = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ];
 
-                    for (var j = 0, numberOfWords = words.length; j < numberOfWords; j += 1) {
-                        if (words[j][0] === word) {
-                            matched = true;
-                            words[j][1] += 1;
-                        }
-                    }
-
-                    if (!matched) {
-                        words.push([word, 1]);
-                    }
-
-                }
-            }*/
             for (var i = 0; i < text.length; i += 1){
-                if (text[i].length>1){ //!(text[i] in stopwords)){
-                words.push(text[i]);
+                if (text[i].length>1){
+                    if (!(text[i] in stopwords)){
+                        words.push(text[i]);
+                    }
                 }
             }
         }
