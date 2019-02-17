d0f70adbc3cd51166e4ceb4d791d6169fdf408fa
BaalajiVS <v.s.baalaji95@gmail.com>
2019-02-16 20:01:45 -0500

    length of unidentified not found

--- Find.js
+++ Find.js
@@ -70,20 +70,40 @@
             //https://github.com/Yoast/YoastSEO.js/blob/develop/src/config/stopwords.js
             var stopwords = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ];
 
-            for (var i = 0; i < text.length; i += 1){
-                if (text[i].length>1){
-                    if (!(text[i] in stopwords)){
+            for (var i = 0; i < 2; i += 1){
+                var word = text[i];
+                if (word.length>1){
+                    if (!(word in stopwords)){
                         var xmlHttp = new XMLHttpRequest();
 
-                        var query = text[i];
+                        var query = word;
                         var Key = "bb8e2c00b2424ba8b09b7978b504a0da";
                         var theUrl = "https://api.wegmans.io/products/search?query=" + query + "&api-version=2018-10-18&subscription-key=" + Key;
                         xmlHttp.open("GET", theUrl, true); // true for asynchronous
                         xmlHttp.send(null);
                         var headers = xmlHttp.getAllResponseHeaders();
-                        console.log(text[i],headers);
+                        console.log(word,headers);
                         if (headers.length>0){
-                            words.push(text[i]);
+                            //words.push(text[i]);
+                            if (!(new RegExp('\\b(' + word + ')\\b', 'g')==null)){
+                                findKW(
+                                    document.body,
+
+                                    // using \\b to only replace when the term is the whole word
+                                    // e.g. if term is "bbb" then "aabbbccc" will not match
+                                    new RegExp('\\b(' + word + ')\\b', 'g'),
+
+                                    // your replacement function, change URL accordingly
+                                    function (match) {
+                                      var link = document.createElement('a');
+                                      link.href = 'http://google.com/#q=' + match;
+                                      link.target = '_blank';
+                                      link.innerHTML = match;
+                                      return link;
+                                    }
+                                );
+                            }
+
                         }
                     }
                 }
@@ -101,24 +121,4 @@
 
     displayWordList(words);
 
-    for (var i = words.length - 1 ; i>=0 ; i--){
-        if (!(new RegExp('\\b(' + words[i] + ')\\b', 'g')==null)){
-            findKW(
-                document.body,
-
-                // using \\b to only replace when the term is the whole word
-                // e.g. if term is "bbb" then "aabbbccc" will not match
-                new RegExp('\\b(' + words[i] + ')\\b', 'g'),
-
-                // your replacement function, change URL accordingly
-                function (match) {
-                  var link = document.createElement('a');
-                  link.href = 'http://google.com/#q=' + match;
-                  link.target = '_blank';
-                  link.innerHTML = match;
-                  return link;
-                }
-            );
-        }
-    }
 }());
